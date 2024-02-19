310675f8b4768a0e10d7188a416ca1eadc4ef59c
PresentJay <presentj94@gmail.com>
2024-02-20 08:50:46 +0900

    ✨ feat: add user creation and retrieval endpoints
    - Added new endpoints for creating and retrieving user information.
    - Created `UserItemGetResponse`, `UserPostRequest`, and `UserPostResponse` models for the request and response bodies.
    - Implemented the `create_user` and `get_user` methods in the `UserService` class.
    - Added `UserRepository` class for interacting with the database.
    - Created `UserModel` class as the SQLAlchemy model for the `Users` table.
    - Added `Service` class as a base class for services.

--- routers/users/user_controller.py
+++ routers/users/user_controller.py
@@ -1,53 +1,50 @@
 from fastapi import APIRouter, Depends
 from dependencies.database import provide_session
 
+from domains.users.services import UserService
+from domains.users.repositories import UserRepository
+from domains.users.dto import (
+    UserItemGetResponse,
+    UserPostRequest,
+    UserPostResponse,
+)
+
 name = "Users"
 router = APIRouter()
 
 
-@router.post(f"/{name}/login")
-async def login(
-    payload=any,
-    db=Depends(provide_session),
-):
-    pass
-
-
-@router.post(f"/{name}/logout")
-async def logout(
-    payload=any,
-    db=Depends(provide_session),
-):
-    pass
-
-
-@router.post(f"/{name}/checkid")
-async def checkid(
-    payload=any,
-    db=Depends(provide_session),
-):
-    pass
-
-
-@router.post(f"/{name}/delete")
-async def delete(
-    payload=any,
-    db=Depends(provide_session),
-):
-    pass
-
-
 @router.post(f"/{name}/create")
 async def create(
-    payload=any,
+    payload=UserPostRequest,
     db=Depends(provide_session),
-):
-    pass
+) -> UserPostResponse:
+    user_service = UserService(user_repository=UserRepository(session=db))
+
+    user_id = user_service.create_user(
+        user_name=payload.user_name,
+        user_pw=payload.user_password,
+    )
+
+    return UserPostResponse(id=user_id)
 
 
 @router.get(f"/{name}/{{user_id}}")
 async def get(
     user_id,
     db=Depends(provide_session),
-):
-    pass
+) -> UserItemGetResponse:
+    user_service = UserService(user_repository=UserRepository(session=db))
+
+    user_info = user_service.get_user(user_id=user_id)
+
+    return UserItemGetResponse(
+        data=UserItemGetResponse.DTO(
+            id=user_info.id,
+            name=user_info.name,
+            flavor_genre_first=user_info.flavor_genre_first,
+            flavor_genre_second=user_info.flavor_genre_second,
+            flavor_genre_third=user_info.flavor_genre_third,
+            created_at=user_info.created_at,
+            updated_at=user_info.updated_at,
+        )
+    )
